f577c8a6c93d228b764a2b1645b10098250e2925
DunEarl <dauthman231@gmail.com>
2021-04-18 10:37:13 +0000

    new assignment

---
+++ users.js
@@ -0,0 +1,22 @@
+const users =[
+    {
+        id: 1, 
+        username: "Kwabena",
+        password: "tuesday",
+        name: "Kwabena Owusu"
+    },
+    {
+        id: 2, 
+        username: 'Kofi',
+        password: 'friday',
+        name: 'Kofi Owusu'
+    },
+    {
+        id: 3, 
+        username: 'Kojo',
+        password: 'sunday',
+        name: 'Kojo Owusu'
+    }
+    
+]
+module.exports = users;
